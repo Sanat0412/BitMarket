d2e5a725e0b14ec6ddd7b138341dffe66360c48e
boby chaudhary <bobychaudhary708@gmail.com>
2021-10-18 15:30:26 +0530

    .

--- bitmarket/src/index.js
+++ bitmarket/src/index.js
@@ -1,3 +1,4 @@
-import ReactDOM from "react-dom";
+import reactDom from "react-dom";
+import React from "react";
 import App from "./App";
-ReactDOM.render(<App />, document.getElementById("root"));
+reactDom.render(<App />, document.getElementById("root"));
