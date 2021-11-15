0decfb217cba1f7a617c2ebf20e5f5625202d718
boby chaudhary <bobychaudhary708@gmail.com>
2021-11-15 12:08:49 +0530

    10k files has been added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Footer = () => {
-  
   return (
     <div>
       <footer class="page-footer font-small unique-color-dark">
@@ -35,24 +34,16 @@ const Footer = () => {
         <div className="container text-center text-md-left mt-5">
           <div className="row mt-3">
             <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              <h6 className="text-uppercase font-weight-bold">Company name</h6>
-              <hr
-                className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto"
-                
-              />
+              <h6 className="text-uppercase font-weight-bold">BitMarket</h6>
+              <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" />
               <p>
-                Here you can use rows and columns to organize your footer
-                content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit.
+                 BitMarket is on a mission to accelerate the world's  transition to cryptocurrency. Through  the BitMarket website, you can analyse many cryptocurrencies.
               </p>
             </div>
 
             <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
               <h6 className="text-uppercase font-weight-bold">Products</h6>
-              <hr
-                className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto"
-                
-              />
+              <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" />
               <p>
                 <a href="#!">MDBootstrap</a>
               </p>
@@ -69,10 +60,7 @@ const Footer = () => {
 
             <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
               <h6 className="text-uppercase font-weight-bold">Useful links</h6>
-              <hr
-                className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto"
-                
-              />
+              <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" />
               <p>
                 <a href="#!">Your Account</a>
               </p>
@@ -89,10 +77,7 @@ const Footer = () => {
 
             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
               <h6 className="text-uppercase font-weight-bold">Contact</h6>
-              <hr
-                className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto"
-                
-              />
+              <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" />
               <p>
                 <i className="fas fa-home mr-3"></i> New York, NY 10012, US
               </p>
