0558b6e66e3b3044f1c416e06942c547d320f67a
bobychaudhary <bobychaudhary708@gmail.com>
2021-11-17 19:15:12 +0530

    .

--- src/components/AnotherApi.js
+++ src/components/AnotherApi.js
@@ -9,7 +9,8 @@ const AnotherApi = () => {
         `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C%207d%2C%20%2030d`
       );
       console.log(res.data);
-    }
+    } 
+    
     getData();
   }, []);
   return <div></div>;
